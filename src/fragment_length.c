2a66bf402be1010ea392554edbefc6594a431d6e
nboley <npboley@gmail.com>
2010-04-22 02:38:58 +0000

    Added fragment length estimation code

--- src/fragment_length.c
+++ src/fragment_length.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <limits.h>
+#include <math.h>
 #include <assert.h>
 
 #include "fragment_length.h"
 #include "config.h"
+#include "mapped_read.h"
 
 void
 init_fl_dist( struct fragment_length_dist_t** fl_dist, int min_fl, int max_fl )
@@ -65,6 +68,143 @@ init_fl_dist_from_file( struct fragment_length_dist_t** fl_dist, FILE* fp )
     fseek( fp, 0, SEEK_SET );
     return;
 }
+
+
+void
+estimate_fl_dist_from_mapped_reads(  struct mapped_reads_db* rdb,
+                                     struct fragment_length_dist_t** fl_dist )
+{
+    /* initialize a temporary fl dist */
+    int total_num_reads = 0;
+    int max_fl = TEMP_FL_ARRAY_GF;
+    int* temp_fls = calloc(sizeof(int), max_fl+1);
+    if( temp_fls == NULL )
+    {
+        fprintf( stderr, "FATAL        : Failed to allocate in estimate fl dist.\n" );
+        exit( -1 );
+    }
+    
+    /* loop through each read */
+    struct mapped_read_t* rd;
+    rewind_mapped_reads_db( rdb );
+
+    while( !mapped_reads_db_is_empty( rdb ) )
+    {
+        get_next_read_from_mapped_reads_db( rdb, &rd );
+        
+        /* If there is exactly one mapping, then we will use it */
+        if( 1 == rd->num_mappings )
+        {
+            int fl = rd->locations[0].stop_pos \
+                   - rd->locations[0].start_pos + 1;
+            /* add this length to the fl dist */
+            if( fl > max_fl )
+            {
+                /* allocate space for the new data */
+                temp_fls = realloc( temp_fls, sizeof(int)*(fl+1) );
+                /* zero out the new entries */
+                memset( temp_fls + max_fl + 2, 0, sizeof(int)*(fl-max_fl) );
+                max_fl = fl;
+            }
+
+            total_num_reads += 1;
+            temp_fls[ fl ] += 1;
+        }
+        
+        free_mapped_read( rd );
+    }
+    
+    /* do a pass over the data to determine the maximum and minimum fls */
+    /* 
+     *  NOTE THAT WE
+     *  trim off the top and bottom 1% of cnts - this will bias the estimate 
+     *  a bit, but I dont know what else to do. Really long fragments good
+     *  realy bias the results 
+     */
+
+    int new_max_fl = 0;
+    int new_min_fl = max_fl;
+    int curr_cnt = 0;
+    int truncated_cnt = 0;
+    double mean = 0;
+    int fl;
+    for( fl = 0; fl < max_fl; fl++ )
+    {
+        if( temp_fls[fl] == 0 )
+            continue;
+        curr_cnt += temp_fls[fl];
+        if( curr_cnt < (int) (LOWER_FL_CUTOFF_QUANTILE*total_num_reads) )
+            continue;
+        if( curr_cnt > (int) (UPPER_FL_CUTOFF_QUANTILE*total_num_reads+0.5) )
+            continue;
+        mean += fl*temp_fls[fl];
+        truncated_cnt += temp_fls[fl];
+        
+        new_min_fl = MIN( fl, new_min_fl );
+        new_max_fl = MAX( fl, new_max_fl );
+    }
+    /* make the sum the mean. */
+    mean = mean/truncated_cnt;
+    
+    /* 
+     * If there are too few fragments, we assume a truncated normal 
+     * density with sample mean and variance. It's not very good, but 
+     * it's probably better than use what is, potentially, very biased
+     * empirical estiamte. 
+     */
+    /* insist on some level of average fragment */
+    if( (new_max_fl - new_min_fl + 1)*MIN_EMPIRICAL_COVERAGE
+        > total_num_reads*(UPPER_FL_CUTOFF_QUANTILE-LOWER_FL_CUTOFF_QUANTILE) )
+    {
+        double emp_variance = 0;
+        /* estimate the variance */
+        for( fl = new_min_fl; fl <= new_max_fl; fl++ )
+        {
+            emp_variance += temp_fls[fl]*(fl-mean)*(fl-mean);
+        }
+        emp_variance /= truncated_cnt;
+        double std_dev = sqrt( emp_variance );
+        
+        /* make the new dist a normal treuncated at 2 stds */
+        new_min_fl = MAX( 0, mean - 2*std_dev );
+        new_max_fl = mean + 2*std_dev;
+        
+        double trace_sum = 0;
+        init_fl_dist( fl_dist, new_min_fl, new_max_fl );
+        for( fl = new_min_fl; fl <= new_max_fl; fl++ )
+        {
+            /* add something proportional the normal pdf */
+            (*fl_dist)->density[fl-new_min_fl] 
+                = (1/std_dev)*exp( 
+                    -(fl-mean)*(fl-mean)/(2*emp_variance)
+                );
+            trace_sum += (*fl_dist)->density[fl-new_min_fl];
+        }
+        /* renormalize to 1 */
+        for( fl = new_min_fl; fl <= new_max_fl; fl++ )
+        {
+            (*fl_dist)->density[fl-new_min_fl] /= trace_sum;
+        }
+        
+        goto cleanup;
+    }
+    /* If we have enough for the empirical estimate */
+    else {
+        /* TODO - maybe some kernel smoothing? */
+        init_fl_dist( fl_dist, new_min_fl, new_max_fl );
+        for( fl = new_min_fl; fl <= new_max_fl; fl++ )
+        {
+            (*fl_dist)->density[ fl - new_min_fl  ] 
+                = (float)temp_fls[fl]/total_num_reads;
+        }
+        
+        goto cleanup;
+    }
+
+cleanup:
+    free( temp_fls );
+}
+
 
 float
 get_fl_prb( struct fragment_length_dist_t* fl_dist, int fl )
--- src/fragment_length.h
+++ src/fragment_length.h
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+#define LOWER_FL_CUTOFF_QUANTILE 0.001
+#define UPPER_FL_CUTOFF_QUANTILE 0.99
+#define MIN_EMPIRICAL_COVERAGE 50
+#define TEMP_FL_ARRAY_GF 100
+
 struct fragment_length_dist_t {
     int min_fl;
     int max_fl;
