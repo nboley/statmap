2e4a8f07f2b8c6d8136ecfcc1365695ac86e14c3
nboley <npboley@gmail.com>
2010-08-25 21:51:06 +0000

    Added missing file.

---
+++ src/peak_calling.h
@@ -0,0 +1,12 @@
+
+#ifndef PEAK_CALLING_H
+
+#define PEAK_CALLING_H
+
+void
+call_peaks_at_local_maxima( struct genome_data* genome, char* samples_dname );
+
+void
+call_peaks( struct genome_data* genome );
+
+#endif
