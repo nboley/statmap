ff42e510c30866e1b7129514b3ef5e2e7b2e98a6
Garrett Robinson <garrett.f.robinson@gmail.com>
2012-06-19 15:39:57 -0700

    Update error data plotting utility to use new log file format

--- utilities/plot_error_data.py
+++ utilities/plot_error_data.py
@@ -8,14 +8,14 @@ import sys
 import os
 from subprocess import call
 
+from itertools import izip
+
 import matplotlib as mpl
 #mpl.use('Agg') # use if X is not installed/for headless plot generation
 import matplotlib.pyplot as plt
 
 # Information about quality scores (Phred scores) for reads from different
 # sequencers
-
-#QUALITY_SCORE_RANGE = range()
 
 FORMAT_TYPES = [
     { 'QUAL_SHIFT': 33, 'ARE_LOG_ODDS': False },
@@ -113,25 +113,51 @@ class ErrorDataStruct():
     Stores error information from a single error_data struct
     '''
 
-    def __init__(self):
-        self.num_unique_reads       = 0
-        self.max_read_length        = 0
-        self.loc_error_rates        = []
-        self.qual_score_error_rates = []
+    def __init__(self, fp=None):
 
-    def __str__(self):
-        lines = []
-        lines.append("Num Unique Reads:\t%i" % self.num_unique_reads)
-        lines.append("Max Read Length:\t%i" % self.max_read_length)
-        lines.append("Loc Error Rates:")
-        for i, ler in enumerate(self.loc_error_rates):
-            lines.append("%i\t%i" % (i, ler))
-        lines.append("Qual Score Error Rates:")
-        for i, qser in enumerate(self.qual_score_error_rates):
-            lines.append("%i\t%i" % (i, qser))
+        # initialize instance vars
+        self.num_unique_reads           = 0
+        self.max_read_length            = 0
+        self.position_mismatch_cnts     = []
+        self.qual_score_cnts            = []
+        self.qual_score_mismatch_cnts   = []
 
-        return '\n'.join(lines)
-        
+        if fp:
+            self._load_from_fp(fp)
+
+    def _load_from_fp(self, fp):
+        """
+        Load error data struct given a fp that is on the first line of an
+        error data struct description
+        """
+        # make sure we are at the start of a new struct
+        line = fp.readline().strip() # remove newline
+        if line.startswith("num_unique_reads"):
+            self.num_unique_reads = int(line.split()[1])
+        else:
+            raise RuntimeError("Error Log fp misaligned")
+
+        # read the next line for the max_read_length
+        line = fp.readline().strip()
+        self.max_read_length = int(line.split()[1])
+
+        # read the position_mismatch_cnts
+        for i in range(self.max_read_length):
+            line = fp.readline().strip()
+            self.position_mismatch_cnts.append(
+                    float(line.split()[1])
+                )
+
+        # read the quality score counts
+        max_num_qual_scores = 256
+        for i in range(max_num_qual_scores):
+            line = fp.readline().strip()
+            self.qual_score_cnts.append(
+                    float(line.split()[2])
+                )
+            self.qual_score_mismatch_cnts.append(
+                    float(line.split()[1])
+                )
 
 class ErrorStatsLog():
     '''
@@ -140,35 +166,24 @@ class ErrorStatsLog():
     
     def __init__(self, fp):
         # parse a list of ErrorDataStructs from error_stats.log
-        self.structs = []
-        self._load_from_file( fp )
+        self.data = []
+        self._load_from_file(fp)
 
-    def _load_from_file( self, fp ):
+    def _load_from_file(self, fp):
         '''
         Read error_stats.log, store each set of error data as an ErrorDataStruct
         '''
-        #index = -1
-        struct = None
-        curr_err_type = None
-        for line in fp:
-            if line.startswith("Num Unique Reads:"):
-                struct = ErrorDataStruct() # make a new struct
-                self.structs.append( struct ) # append to list of structs
-                struct.num_unique_reads = int(line.split()[-1].strip()) # set num_unique_reads
-            elif line.startswith("Max Read Length:"):
-                struct.max_read_length = int(line.split()[-1].strip())
-            elif line.startswith("Loc Error Rates:"):
-                curr_err_type = "loc"
-            elif line.startswith("Qual Score Error Rates:"):
-                curr_err_type = "qual"
+        # loop over file, loading structs in one at a time
+        # after every struct, read the next line in to test for EOF
+        while True:
+            pos = fp.tell()
+            line = fp.readline() # returns "" on EOF
+            if line:
+                # return to original position
+                fp.seek(pos)
+                self.data.append( ErrorDataStruct(fp) )
             else:
-                if curr_err_type == "loc":
-                    struct.loc_error_rates.append( float(line.split()[-1].strip()) )
-                elif curr_err_type == "qual":
-                    struct.qual_score_error_rates.append( float(line.split()[-1].strip()) )
-
-    def __str__(self):
-        return '\n'.join( map(str, self.structs) ) 
+                break
 
 class MappedReads:
     '''
@@ -190,15 +205,27 @@ class MappedReads:
             )
 
 def plot_error_stats( esl, mpdrds ):
-    for edt in esl.structs:
+    for ed in esl.data:
+        # Compute loc_error_rates
+        loc_error_rates = [
+                (pmc / ed.num_unique_reads)
+                for pmc in ed.position_mismatch_cnts
+            ]
         # Plot loc_error_rates
         plt.subplot(211)
-        plt.plot( edt.loc_error_rates )
+        plt.plot( loc_error_rates )
         plt.xlabel("Loc in read"); plt.ylabel("P(mismatch)")
 
+        # Compute qual_score error rates
+        qual_score_error_rates = [
+                (qsm / qsc ) if qsc != 0 else 0
+                for qsm, qsc in
+                izip( ed.qual_score_mismatch_cnts,
+                      ed.qual_score_cnts )
+            ]
         # Plot qual_score_error_rates
         plt.subplot(212)
-        plt.plot( edt.qual_score_error_rates )
+        plt.plot( qual_score_error_rates )
         plt.xlabel("Quality Score"); plt.ylabel("P(mismatch)")
 
 def main():
