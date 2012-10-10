b712fbf22e0e67cbac6db8e1c175d1b16b1cab19
Garrett Robinson <garrett.f.robinson@gmail.com>
2012-10-09 19:55:02 -0700

    Bugfix - use proper strand in track name

--- utilities/split_wig_by_tracks.py
+++ utilities/split_wig_by_tracks.py
@@ -5,13 +5,14 @@ import sys
 track_name = sys.argv[1]
 
 fp = None
-fps = [ open( track_name + ".plus.wig", "w" ), open( track_name + ".minus.wig", "w" ) ]
+fps = [ (open( track_name + ".plus.wig", "w" ), "plus"),
+        (open( track_name + ".minus.wig", "w" ), "minus") ]
 for line in sys.stdin:
     if line.startswith( 'track' ):
         if fp != None:
             fp.close()
-        fp = fps.pop(0)
-        fp.write( "track type=wiggle_0 name=%s_plus\n" % track_name )
+        fp, strand = fps.pop(0)
+        fp.write( "track type=wiggle_0 name=%s_%s\n" % (track_name, strand) )
         continue
     fp.write( line )
 
